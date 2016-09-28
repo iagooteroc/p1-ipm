83074447dd63a767680221a1e3173c92b995886e
iagooteroc <iago_neosky@hotmail.com>
2016-09-28 11:00:55 +0200

    Commit

--- p1.py
+++ p1.py
@@ -10,7 +10,7 @@ film_list = [("Ejemplo1", "Desc1"),
 class TreeViewFilterWindow(Gtk.Window):
 
     def __init__(self):
-        Gtk.Window.__init__(self, title="Lista de películas")
+        Gtk.Window.__init__(self, title="Lista de pelculas")
         self.set_border_width(10)
 
         #Setting up the self.grid in which the elements are to be positionned
@@ -27,7 +27,7 @@ class TreeViewFilterWindow(Gtk.Window):
         #creating the treeview and adding the columns
         self.treeview = Gtk.TreeView.new_with_model(self.film_liststore)
 
-        for i, column_title in enumerate(["Nombre","Desc"]):
+        for i, column_title in enumerate(["Nombre","Descripcin"]):
             renderer = Gtk.CellRendererText()
             renderer.set_property("editable",True)
             column = Gtk.TreeViewColumn(column_title, renderer, text=i)
@@ -41,15 +41,31 @@ class TreeViewFilterWindow(Gtk.Window):
         self.grid.attach(self.scrollable_treelist, 0, 0, 2, 5)
         self.scrollable_treelist.add(self.treeview)
         
+        # Adding the Add button
+        self.add_button = Gtk.Button.new_with_label("Añadir")
+        self.add_button.connect("clicked", self.on_add_clicked)
+        self.grid.attach_next_to(self.add_button, self.scrollable_treelist, Gtk.PositionType.BOTTOM, 1, 1)
+        
         # Adding the Remove button
-        self.button = Gtk.Button.new_with_label("Eliminar")
-        self.button.connect("clicked", self.on_remove_clicked)
-        self.grid.attach_next_to(self.button, self.scrollable_treelist, Gtk.PositionType.BOTTOM, 1, 1)
+        self.remove_button = Gtk.Button.new_with_label("Eliminar")
+        self.remove_button.connect("clicked", self.on_remove_clicked)
+        self.grid.attach_next_to(self.remove_button, self.add_button, Gtk.PositionType.RIGHT, 1, 1)
 
         self.show_all()
 
     def text_edited(self, widget, path, text, model=None, column=0):
         self.film_liststore[path][column] = text
+        
+    def on_add_clicked(self, widget): 
+        add = DialogAdd(self)
+        response = add.run()
+        
+        if response == Gtk.ResponseType.OK:
+            print("Ok")
+        add.destroy()
+    
+    def add_film(self, widget, name):
+        self.film_liststore.append(list((name, "SampleDescription")))
 
     def on_remove_clicked(self, widget):
         selection = self.treeview.get_selection()
@@ -63,6 +79,23 @@ class TreeViewFilterWindow(Gtk.Window):
                 model.remove(iter)
             warning.destroy()
 
+class DialogAdd(Gtk.Dialog):
+    def __init__(self, parent):
+        Gtk.Dialog.__init__(self, "Add", parent, 0,
+            ("Aceptar", Gtk.ResponseType.OK,
+             "Cancelar", Gtk.ResponseType.CANCEL))
+        self.set_default_size(150, 100)
+        self.set_modal(True)
+        entry = Gtk.Entry()
+        box = self.get_content_area()
+        box.add(entry)
+        # Se puede poner el Enter como Aceptar: entry.connect("activate", 
+        ########TODO########
+        # Esto tiene que hacerse despus de darle a Aceptar:
+        #new_text = entry.get_text()
+        #parent.add_film(parent, new_text)
+        self.show_all() # igual no hace falta
+
 class DialogWarning(Gtk.Dialog):
 
     def __init__(self, parent, text):
@@ -73,7 +106,7 @@ class DialogWarning(Gtk.Dialog):
         self.set_default_size(150, 100)
         self.set_modal(True)
 
-        label = Gtk.Label("¿Seguro que desea eliminar la película ''" + text+ "''?")
+        label = Gtk.Label("¿Seguro que desea eliminar la pelcula ''" + text+ "''?")
 
         box = self.get_content_area()
         box.add(label)
@@ -83,3 +116,4 @@ win = TreeViewFilterWindow()
 win.connect("delete-event", Gtk.main_quit)
 win.show_all()
 Gtk.main()
+
